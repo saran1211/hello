4cfff4c7ee4e8fd33b3602ffb22b6220a02d4e84
saran1211 <36408495+saran1211@users.noreply.github.com>
2018-02-15 15:50:08 +0530

    Update romval.py

--- romval.py
+++ romval.py
@@ -1,12 +1,8 @@
-class py_solution:
-
-    def roman_to_int(self, s):
-
-        rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-
-        int_val = 0
-
-        for i in range(len(s)):
+class py_soln:
+def roman_to_int(self, s):
+    rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    int_val = 0
+for i in range(len(s)):
 
             if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
 
