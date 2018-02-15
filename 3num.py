36091021f1bd941b5b0750cb96bc52130f96fbd6
saran1211 <36408495+saran1211@users.noreply.github.com>
2018-02-15 15:07:48 +0530

    3num
    largest among 3 nums

---
+++ 3num.py
@@ -0,0 +1,21 @@
+num1 = int(input("Enter first num: "))
+
+num2 = int(input("Enter second num: "))
+
+num3 = int(input("Enter third num: "))
+
+
+if (num1 >= num2) and (num1 >= num3):
+
+   largest = num1
+
+elif (num2 >= num1) and (num2 >= num3):
+
+   largest = num2
+
+else:
+
+   largest = num3
+
+
+print("The largest number between",num1,",",num2,"and",num3,"is",largest)
